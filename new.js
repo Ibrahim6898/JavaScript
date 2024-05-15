77ffe6ef46af9c7f9ccc5f4405d14517cdeed278
ibrahim6898 <ibrahimyakubuahmad58@gmail.com>
2024-05-15 11:57:45 +0100

    javascript_learning

---
+++ new.js
@@ -0,0 +1,80 @@
+console.log("hello world")
+var number = 5; // in-line comment
+
+/*
+multi line commentvc
+ */
+
+var myName = "Yusuf"
+
+myName = 8
+
+let urName = "computer engineering"
+
+const pi = 3.14
+
+a = 7;
+b = a;
+
+console.log(a)
+
+var a = 5;
+var b = 10;
+var c = "I am a "
+
+a = a + 1;
+b = b + 5;
+c = c + "String!"
+
+console.log(a)
+console.log(b)
+console.log(c)
+
+var sum = 10 + 10
+console.log(sum)
+
+var myVar = 87;
+myVar = myVar + 1;
+myVar++
+console.log(myVar)
+myVar++
+console.log(myVar)
+
+myCode = 10;
+myCode = myCode - 1;
+//myVar++
+console.log(myCode)
+myCode++
+console.log(myCode)
+
+var a = 3;
+var b = 17
+var c = 12
+
+a = a + 12
+b = b + 9
+c = c + 7
+
+console.log(a,b,c)
+
+a +=12
+b += 9
+c += 7
+console.log(a,b,c)
+
+a -= 12
+b -= 9
+c -=7
+console.log(a,b,c)
+
+var myStr =  "I am a \"double quoted\" string inside \"double quotes\"."
+console.log(myStr)
+
+var myStr1 =  'I am a "double quoted" string inside "double quotes".'
+console.log(myStr1)
+
+var myStr2 =  "FirstLine\n\t\SecondLine\nThirdLine"
+console.log(myStr2)
+
+var myName = "My name is " + "Ibrahim"
+console.log(myName)
