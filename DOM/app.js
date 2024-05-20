4aa70500551d6404b463d457b2adedfccde459a7
ibrahim6898 <ibrahimyakubu58@gmail.com>
2024-05-20 17:11:30 +0100

    manipulation

---
+++ DOM/app.js
@@ -0,0 +1,19 @@
+//DOM Manipulation
+
+//getElemntById()
+const title = document.getElementById("main-heading");
+console.log(title);
+
+//getElementByClassName()
+const listItem = document.getElementsByClassName("list-items");
+console.log(listItem);
+
+//getElementByTagName()
+const list = document.getElementsByTagName("li");
+console.log(list);
+
+//querySelector()
+const container = document.querySelector("div");
+console.log(container)
+
+//querySelectorAll()
